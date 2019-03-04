1401b66bf26ca4046a1b5344e671517696b13135
League <league@WTS9.attlocal.net>
2019-03-03 17:24:48 -0800

    Body Part Quiz

--- src/BodyPartQuiz.java
+++ src/BodyPartQuiz.java
@@ -31,7 +31,7 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+int score = 0;
 		// 2. Set the size of the window in the initializeGui() method below
 			
 		// 4. Ask the user who this person is and store their answer
@@ -39,7 +39,11 @@ public class BodyPartQuiz {
 
 		// 5. Check their answer. If they guessed correctly:
 		// -- Tell them they are right and increase the score by 1
-
+if (guess=="arnold") {
+	System.out.println("Correct!");
+} else if (guess=="leonardo") {
+	System.out.println("");
+}
 		// 6. Otherwise:
 		// -- Tell them they are wrong and who the person is
 
