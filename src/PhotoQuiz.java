83e8dbeff3af433ff51ecec3de93c63551e73148
League <league@WTS9.attlocal.net>
2019-05-05 17:27:23 -0700

    Photo Quiz

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,21 +22,24 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+String fairy = "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjAp7mbxIXiAhXsJDQIHcb-ChwQjRx6BAgBEAU&url=https%3A%2F%2Ftwitter.com%2Fpromsienn%2Fstatus%2F879054051496906752&psig=AOvVaw1khX58apkW1lAmU8xv19Lx&ust=1557184569814805";
 		// 2. create a variable of type "Component" that will hold your image
-
+Component image = createImage(fairy);
 		// 3. use the "createImage()" method below to initialize your Component
-
 		// 4. add the image to the quiz window
-
+quizWindow.add(image);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String quesh = JOptionPane.showInputDialog("why is Kendra Sorenson crying?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if(quesh.equals("she is scared for her family")){
+	JOptionPane.showMessageDialog(null, "CORRECT!!!!");
+}
 		// 8. print "INCORRECT" if the answer is wrong
-
+else {
+	JOptionPane.showMessageDialog(null, "WRONG!!!!");
+}
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
 
 		// 10. find another image and create it (might take more than one line of code)
