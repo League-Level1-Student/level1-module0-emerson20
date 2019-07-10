7271a7ac6bf0c0dd7e708fcfaa3740fad3b7fe02
League Student <league@iMac-14.attlocal.net>
2019-07-10 16:20:34 -0700

    Sound Effects

--- src/SoundEffect.java
+++ src/SoundEffect.java
@@ -1,4 +1,60 @@
+import java.applet.AudioClip;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SoundEffect {
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SoundEffect implements ActionListener{
+	public static void main(String[] args) {
+		
+	
+	//1. Create a GUI for a sound effects machine that will play different sounds when the buttons are pressed.
+    //If you need help, take a look at the instructions for the Fortune Cookie recipe.
+		//NOTE: because this GUI has more than one button, you must add a JPanel to your JFrame and put the buttons on the panel! 
+	JFrame frame = new JFrame();
+	   frame.setVisible(true);
+	   
+	   JPanel pat = new JPanel();
+	   pat.setVisible(true);
+	   
+	     frame.add(pat);
+	     
+	   JButton button = new JButton("water");
+	   button.addActionListener(this);
+	   JButton boa = new JButton();
+	   JButton boat = new JButton();
+	   JButton put = new JButton();
+	   JButton pet = new JButton();
+	   
+	    pat.add(button);
+	    pat.add(boa);
+	    pat.add(boat);
+	    pat.add(put);
+	    pat.add(pet);
+	    frame.pack();
+	    
+  
+
+//2. Gather sound bites. They must be .wav files. Free sound effects can be found here. The League also has an account at freesound. Ask your teacher for our login.
+
+//The .wav files must be placed into your Java project (drag and drop).
+
+//3. Call this playSound() method when they click on a button. You will need to import java.applet.AudioClip 
+
+	}
+private void playSound(String fileName) {
+   AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName)); 
+   sound.play();
+    
+
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
+}
 
 }
