2fa24f5c01378189910f26c78693b399e4765110
League Student <league@iMac-16.attlocal.net>
2019-07-28 17:28:52 -0700

    Sound Effect

--- src/SoundEffect.java
+++ src/SoundEffect.java
@@ -14,28 +14,7 @@ public class SoundEffect implements ActionListener{
 	//1. Create a GUI for a sound effects machine that will play different sounds when the buttons are pressed.
     //If you need help, take a look at the instructions for the Fortune Cookie recipe.
 		//NOTE: because this GUI has more than one button, you must add a JPanel to your JFrame and put the buttons on the panel! 
-	JFrame frame = new JFrame();
-	   frame.setVisible(true);
-	   
-	   JPanel pat = new JPanel();
-	   pat.setVisible(true);
-	   
-	     frame.add(pat);
-	     
-	   JButton button = new JButton("water");
-	   button.addActionListener(this);
-	   JButton boa = new JButton();
-	   JButton boat = new JButton();
-	   JButton put = new JButton();
-	   JButton pet = new JButton();
-	   
-	    pat.add(button);
-	    pat.add(boa);
-	    pat.add(boat);
-	    pat.add(put);
-	    pat.add(pet);
-	    frame.pack();
-	    
+	
   
 
 //2. Gather sound bites. They must be .wav files. Free sound effects can be found here. The League also has an account at freesound. Ask your teacher for our login.
@@ -55,6 +34,28 @@ private void playSound(String fileName) {
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
 	
+}private void area(){
+	JFrame frame = new JFrame();
+	   frame.setVisible(true);
+	   
+	   JPanel pat = new JPanel();
+	   pat.setVisible(true);
+	   
+	     frame.add(pat);
+	     
+	   JButton button = new JButton("water");
+	   button.addActionListener(this);
+	   JButton boa = new JButton("");
+	   JButton boat = new JButton("");
+	   JButton put = new JButton("");
+	   JButton bet = new JButton("");
+	   
+	    pat.add(button);
+	    pat.add(boa);
+	    pat.add(boat);
+	    pat.add(put);
+	    pat.add(bet);
+	    frame.pack();
 }
 
 }
